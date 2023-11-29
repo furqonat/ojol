fc95ee4af55e9d514fa4c7b1a2a2b26f12cafe44
furqonat <furqonr.atthoriq@gmail.com>
2023-11-29 19:16:48 +0700

    menambahkan dokumentasi untuk autentikasi pengguna aplikasi lugo

--- apps/account/src/app/admin/admin.module.ts
+++ apps/account/src/app/admin/admin.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common'
 import { AdminController } from './admin.controller'
 import { AdminService } from './admin.service'
 import { FirebaseService } from '@lugo/firebase'
+import { ConfigService } from '@nestjs/config'
 
 @Module({
-  providers: [AdminService, UsersPrismaService, FirebaseService],
+  providers: [AdminService, UsersPrismaService, FirebaseService, ConfigService],
   controllers: [AdminController],
 })
 export class AdminModule {}
--- libs/transactions-lib/jest.config.ts
+++ libs/transactions-lib/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'users',
+  displayName: 'transaction-lib',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../coverage/users',
+  coverageDirectory: '../coverage/transaction-lib',
 }
