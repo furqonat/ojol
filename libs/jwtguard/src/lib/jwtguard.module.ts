bc0d9de31aeac7dbde4654efae7cc57981685aaf
furqonat <furqonr.atthoriq@gmail.com>
2023-12-04 14:11:40 +0700

    add config module to config module

--- libs/jwtguard/src/lib/jwtguard.module.ts
+++ libs/jwtguard/src/lib/jwtguard.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common'
 import { APP_GUARD } from '@nestjs/core'
 import { JwtService } from '@nestjs/jwt'
 import { RolesGuard } from './roles.guard'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [
     {
