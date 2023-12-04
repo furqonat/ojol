b6298a64d2caca8044e6b8adbe16d9088cb5f5f5
furqonat <furqonr.atthoriq@gmail.com>
2023-12-04 13:47:37 +0700

    add jwt register to admin module

--- apps/auth/src/app/admin/admin.module.ts
+++ apps/auth/src/app/admin/admin.module.ts
@@ -1,12 +1,22 @@
 import { BcryptModule, BcryptService } from '@lugo/bcrypt'
 import { UsersPrismaService } from '@lugo/users'
 import { Module } from '@nestjs/common'
-import { JwtService } from '@nestjs/jwt'
+import { JwtModule, JwtService } from '@nestjs/jwt'
 import { AdminController } from './admin.controller'
 import { AdminService } from './admin.service'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [BcryptModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      global: true,
+    }),
+    BcryptModule,
+  ],
   providers: [AdminService, UsersPrismaService, BcryptService, JwtService],
   controllers: [AdminController],
 })
