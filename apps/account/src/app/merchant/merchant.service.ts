10830772627659fdf6ecc3c41639878f961de277
furqonat <furqonr.atthoriq@gmail.com>
2023-12-06 15:04:23 +0700

    add operational time for mechant

--- apps/account/src/app/merchant/merchant.service.ts
+++ apps/account/src/app/merchant/merchant.service.ts
@@ -69,4 +69,64 @@ export class MerchantService {
       throw new BadRequestException({ message: 'Internal Error' })
     }
   }
+
+  async createOperationTime(
+    token: string,
+    data: Omit<
+      Prisma.merchant_operation_timeCreateInput,
+      'id' | 'merchant_details'
+    >,
+  ) {
+    try {
+      const decodeToken = await this.firebase.auth.verifyIdToken(token)
+      const merchant = await this.prismaService.merchant.findUnique({
+        where: {
+          id: decodeToken.uid,
+        },
+        select: {
+          details: {
+            select: {
+              id: true,
+            },
+          },
+        },
+      })
+      if (merchant.details.id) {
+        const details = await this.prismaService.merchant_details.update({
+          where: {
+            id: merchant.details.id,
+          },
+          data: {
+            operation_time: {
+              create: data,
+            },
+          },
+        })
+        return {
+          message: 'OK',
+          res: details.id,
+        }
+      } else {
+        throw new BadRequestException()
+      }
+    } catch (e) {
+      throw new BadRequestException(e)
+    }
+  }
+
+  async updateOperationTime(
+    opId: string,
+    data: Prisma.merchant_operation_timeUpdateInput,
+  ) {
+    const res = this.prismaService.merchant_operation_time.update({
+      where: {
+        id: opId,
+      },
+      data: data,
+    })
+    return {
+      message: 'OK',
+      res: res,
+    }
+  }
 }
