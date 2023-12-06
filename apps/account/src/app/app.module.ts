2cbbf1e2ad28f70c3379d2defa0ea81dc5055ba2
furqonat <furqonr.atthoriq@gmail.com>
2023-12-06 15:56:22 +0700

    restrucure project folder account

--- apps/account/src/app/app.module.ts
+++ apps/account/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { JwtGuardModule } from '@lugo/jwtguard'
 import { UsersModule } from '@lugo/users'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { AdminModule } from './admin/admin.module'
-import { CustomerModule } from './customer/customer.module'
+import { AdminModule } from '../admin/admin.module'
+import { CustomerModule } from '../customer/customer.module'
 
 @Module({
   imports: [
---
+++ apps/finance/src/fee/fee.controller.spec.ts
@@ -0,0 +1,1 @@
+describe('Fee Controller test', () => {})
