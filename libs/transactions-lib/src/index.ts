b9605b801e510748396161ec89e1ef0c4c18f2ad
furqonat <furqonr.atthoriq@gmail.com>
2023-12-06 17:00:46 +0700

    add service transactions and service cart

--- libs/transactions-lib/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/transactions.module'
-export * from './lib/transactions.service'
-export { Prisma } from '@prisma/client/transactions'
--- libs/transactions-lib/src/lib/transactions.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-import { TransactionsPrismaService } from './transactions.service'
-
-@Module({
-  controllers: [],
-  providers: [TransactionsPrismaService],
-  exports: [TransactionsPrismaService],
-})
-export class TransactionsModule {}
