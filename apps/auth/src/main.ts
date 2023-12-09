8bc77213c04ff4b2e84cd9dfcd95a5762b6c5651
furqonat <furqonr.atthoriq@gmail.com>
2023-12-09 16:42:21 +0700

    first try reduce cold start nestjs

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,25 +1,31 @@
-import serverlessExpress from '@vendia/serverless-express'
+import { Server } from 'http'
+import { APIGatewayEvent, Context } from 'aws-lambda'
+import * as serverlessExpress from 'aws-serverless-express'
+import express, { Express } from 'express'
+
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app/app.module'
+import { ExpressAdapter } from '@nestjs/platform-express'
 
-import { Handler } from 'express'
-let server: Handler
+let lambdaProxy: Server
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: '*',
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-      preflightContinue: false,
-      optionsSuccessStatus: 200,
-    },
-  })
+  const expressServer: Express = express()
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressServer),
+  )
   app.useGlobalPipes(new ValidationPipe())
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+  })
   await app.init()
-  const expressApp = app.getHttpAdapter().getInstance()
-  return serverlessExpress({ app: expressApp })
+  return serverlessExpress.createServer(expressServer, null, [])
   // const port = process.env.PORT || 3333
   // await app.listen(port)
   // // Logger.log(
@@ -27,10 +33,22 @@ async function bootstrap() {
   // // )
 }
 
-const handler: Handler = async (event, context, callback) => {
-  server = server ?? (await bootstrap())
-  return server(event, context, callback)
+bootstrap().then((server) => (lambdaProxy = server))
+
+function waitForServer(event: APIGatewayEvent, context: Context) {
+  setImmediate(() => {
+    if (!lambdaProxy) {
+      waitForServer(event, context)
+    } else {
+      serverlessExpress.proxy(lambdaProxy, event, context)
+    }
+  })
 }
 
-export { handler }
-/* bootstrap() */
+export const handler = (event: APIGatewayEvent, context: Context) => {
+  if (lambdaProxy) {
+    serverlessExpress.proxy(lambdaProxy, event, context)
+  } else {
+    waitForServer(event, context)
+  }
+}
