78c401a187df59b1afcea30f9237544dd3ee9cca
furqonat <furqonr.atthoriq@gmail.com>
2023-12-11 22:36:22 +0700

    fix error on token

--- apps/product/src/app/app.service.ts
+++ apps/product/src/app/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { FirebaseService } from '@lugo/firebase'
-import { PrismaService } from '@lugo/prisma'
+import { Prisma, PrismaService } from '@lugo/prisma'
 
 @Injectable()
 export class AppService {
@@ -9,5 +9,19 @@ export class AppService {
     private readonly prisma: PrismaService,
   ) {}
 
-  async getProduct() {}
+  async getProduct(productId: string, select: Prisma.productSelect) {
+    return this.prisma.product.findUnique({
+      where: {
+        id: productId,
+      },
+      select: select ? select : { id: true, name: true },
+    })
+  }
+  async getProducts(take: number, skip: number, select: Prisma.productSelect) {
+    return this.prisma.product.findMany({
+      select: select ? select : { id: true, name: true },
+      take: take ? Number(take) : 10,
+      skip: skip ? Number(skip) : 0,
+    })
+  }
 }
