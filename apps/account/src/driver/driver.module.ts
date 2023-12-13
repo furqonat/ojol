ed29258d081c32d97ec8747d35e91ca54967e2da
furqonat <furqonr.atthoriq@gmail.com>
2023-12-13 13:23:48 +0700

    add firebase service

--- apps/account/src/driver/driver.module.ts
+++ apps/account/src/driver/driver.module.ts
@@ -3,9 +3,10 @@ import { PrismaModule } from '@lugo/prisma'
 import { Module } from '@nestjs/common'
 import { DriverController } from './driver.controller'
 import { DriverService } from './driver.service'
+import { FirebaseModule } from '@lugo/firebase'
 
 @Module({
-  imports: [PrismaModule, GuardModule],
+  imports: [PrismaModule, FirebaseModule, GuardModule],
   providers: [DriverService],
   controllers: [DriverController],
 })
