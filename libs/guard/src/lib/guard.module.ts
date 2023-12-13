406548647da623f02d5d8a2b03a2c14482e9f2fc
furqonat <furqonr.atthoriq@gmail.com>
2023-12-13 13:24:38 +0700

    change provider to import

--- libs/guard/src/lib/guard.module.ts
+++ libs/guard/src/lib/guard.module.ts
@@ -1,16 +1,16 @@
+import { FirebaseModule } from '@lugo/firebase'
 import { Module } from '@nestjs/common'
-import { RolesGuard } from './roles.guard'
 import { APP_GUARD } from '@nestjs/core'
-import { FirebaseService } from '@lugo/firebase'
+import { RolesGuard } from './roles.guard'
 
 @Module({
+  imports: [FirebaseModule],
   controllers: [],
   providers: [
     {
       provide: APP_GUARD,
       useClass: RolesGuard,
     },
-    FirebaseService,
   ],
 })
 export class GuardModule {}
