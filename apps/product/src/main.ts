a6bd0c90ff2a71088bba8b3fa0974d7a655d8999
furqonat <furqonr.atthoriq@gmail.com>
2023-12-13 14:45:14 +0700

    add product service deployment

--- apps/product/src/main.ts
+++ apps/product/src/main.ts
@@ -1,22 +1,36 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common'
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
+import serverlessExpress from '@vendia/serverless-express'
+import { Handler, Callback, Context } from 'aws-lambda'
 import { AppModule } from './app/app.module'
+import { ReplaySubject, firstValueFrom } from 'rxjs'
+
+const serverSubject = new ReplaySubject<Handler>()
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  const globalPrefix = 'api'
-  app.setGlobalPrefix(globalPrefix)
-  const port = process.env.PORT || 3000
-  await app.listen(port)
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  )
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      preflightContinue: false,
+      optionsSuccessStatus: 200,
+    },
+    logger: ['error'],
+  })
+  app.useGlobalPipes(new ValidationPipe({ transform: true }))
+  await app.init()
+  const expressApp = app.getHttpAdapter().getInstance()
+  return serverlessExpress({ app: expressApp })
 }
 
-bootstrap()
+bootstrap().then((server) => serverSubject.next(server))
+
+export const handler: Handler = async (
+  event: unknown,
+  context: Context,
+  callback: Callback,
+) => {
+  const server = await firstValueFrom(serverSubject)
+  return server(event, context, callback)
+}
