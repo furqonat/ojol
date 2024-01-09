1b9f0ac31eacf9f724869c5701abe42204421d67
furqonat <furqonr.atthoriq@gmail.com>
2024-01-09 08:06:11 +0700

    add status

--- apps/management/app/notifications/add.notification.tsx
+++ apps/management/app/notifications/add.notification.tsx
@@ -2,6 +2,7 @@
 
 import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage'
 import { useSession } from 'next-auth/react'
+import Image from 'next/image'
 import { useRef, useState } from 'react'
 import Select, { SingleValue } from 'react-select'
 
@@ -21,6 +22,7 @@ export function AddNotification() {
   const { data } = useSession()
 
   const dialogRef = useRef<HTMLDialogElement>(null)
+  const fileRef = useRef<HTMLInputElement>(null)
 
   const [title, setTitle] = useState('')
   const [files, setFiles] = useState<FileList | null>(null)
@@ -41,7 +43,7 @@ export function AddNotification() {
     setTitle(value)
   }
 
-  function handleChangeMinFile(e: React.ChangeEvent<HTMLInputElement>) {
+  function handleChangeFile(e: React.ChangeEvent<HTMLInputElement>) {
     const { files } = e.target
     setFiles(files)
   }
@@ -147,14 +149,23 @@ export function AddNotification() {
                 <span className="label-text-alt">Image (Optional)</span>
               </div>
               <input
-                onChange={handleChangeMinFile}
+                ref={fileRef}
+                onChange={handleChangeFile}
                 required={true}
                 type={'file'}
                 placeholder="Image"
                 className={'file-input file-input-bordered'}
               />
             </label>
-
+            {files && files.length > 0 ? (
+              <PreviewImgFile
+                data={files}
+                onDelete={() => {
+                  fileRef.current!.value = ''
+                  setFiles(null)
+                }}
+              />
+            ) : null}
             <div className={'flex flex-row-reverse mt-4 gap-5'}>
               <button className={'btn btn-sm btn-primary'}>
                 {loading ? <span className={'loading loading-dots'} /> : null}
@@ -175,5 +186,47 @@ export function AddNotification() {
         </form>
       </dialog>
     </>
+  )
+}
+
+function PreviewImgFile(props: {
+  data: FileList
+  onDelete: (index: number) => void
+}) {
+  function handleClickDelete() {
+    props.onDelete(0)
+  }
+  return (
+    <div className={'card shadow-sm rounded-md'}>
+      <div className={'card-body'}>
+        <Image
+          width={200}
+          height={200}
+          src={URL.createObjectURL(props.data.item(0) as Blob)}
+          alt={props.data.item(0)?.name ?? ''}
+        />
+      </div>
+      <div className={'card-actions'}>
+        <button
+          className={'btn-outline btn btn-sm'}
+          onClick={handleClickDelete}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-4 h-4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
   )
 }
