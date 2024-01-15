922fd452e0d550bd08589287bb29ac22350dc0cd
furqonat <furqonr.atthoriq@gmail.com>
2024-01-15 10:22:15 +0700

    add merchant, driver topup

--- apps/gate/services/dana.go
+++ apps/gate/services/dana.go
@@ -166,7 +166,7 @@ func (dana DanaService) RequestWithdraw(phoneNumber string, amount int) (*utils.
 			"fundType": "AGENT_TOPUP_FOR_USER_SETTLE",
 			"fundAmount": map[string]interface{}{
 				"currency": "IDR",
-				"value":    amount * 1000,
+				"value":    amount * 100,
 			},
 			"requestId": guid,
 			"agentMode": "NORMAL",
@@ -191,4 +191,101 @@ func (dana DanaService) RequestWithdraw(phoneNumber string, amount int) (*utils.
 	}
 
 	return &result.Response.Body, nil
+}
+
+func (dana DanaService) CreateNewOrder(expiryTime, transactionType, title, orderID string, amountInCent int, riskObjectID, riskObjectCode, riskObjectOperator, accessToken string) (*utils.CreateOrder, error) {
+	timestam := dana.danaApi.GetDateNow()
+	guid := dana.danaApi.GenerateGUID()
+
+	requestData := map[string]interface{}{
+		"head": map[string]interface{}{
+			"version":      "2.0",
+			"function":     "dana.acquiring.order.createOrder",
+			"clientId":     utils.ClientID,
+			"clientSecret": utils.ClientSecret,
+			"reqTime":      timestam,
+			"reqMsgId":     guid,
+			"accessToken":  accessToken,
+			"reserve":      "{}",
+		},
+		"body": map[string]interface{}{
+			"envInfo": map[string]interface{}{
+				"terminalType":       "SYSTEM",
+				"osType":             "",
+				"extendInfo":         "",
+				"orderOsType":        "",
+				"sdkVersion":         "",
+				"websiteLanguage":    "",
+				"tokenId":            "",
+				"sessionId":          "",
+				"appVersion":         "",
+				"merchantAppVersion": "",
+				"clientKey":          "",
+				"orderTerminalType":  "SYSTEM",
+				"clientIp":           "",
+				"sourcePlatform":     "IPG",
+			},
+			"order": map[string]interface{}{
+				"expiryTime":        expiryTime,
+				"merchantTransType": transactionType,
+				"orderTitle":        title,
+				"merchantTransId":   orderID,
+				"orderMemo":         "",
+				"createdTime":       timestam,
+				"orderAmount": map[string]interface{}{
+					"value":    amountInCent * 100,
+					"currency": "IDR",
+				},
+				"goods": []map[string]interface{}{
+					{
+						"unit":     "",
+						"category": "",
+						"price": map[string]interface{}{
+							"value":    amountInCent,
+							"currency": "IDR",
+						},
+						"merchantShippingId": "",
+						"merchantGoodsId":    "",
+						"description":        title,
+						"snapshotUrl":        "",
+						"quantity":           "",
+						"extendInfo":         fmt.Sprintf("{'objectId':'%s','objectCode':'%s','objectOperator':'%s'}", riskObjectID, riskObjectCode, riskObjectOperator),
+					},
+				},
+			},
+			"productCode": "51051000100000000001",
+			"mcc":         utils.MerchantMCC,
+			"merchantId":  utils.MerchantID,
+			"extendInfo":  "",
+			"notificationUrls": []map[string]interface{}{
+				{
+					"type": "PAY_RETURN",
+					"url":  utils.AcquirementPayReturnURL,
+				},
+				{
+					"type": "NOTIFICATION",
+					"url":  utils.AcquirementNotificationURL,
+				},
+			},
+		},
+	}
+
+	response, err := dana.danaApi.New("/dana/acquiring/order/createOrder.htm", requestData)
+
+	if err != nil {
+		return nil, err
+	}
+
+	result := utils.Result[utils.CreateOrder]{}
+	errParse := json.Unmarshal(response, &result)
+	if errParse != nil {
+		return nil, errParse
+	}
+
+	if result.Response.Body.ResultInfo.ResultCode != "SUCCESS" {
+		return nil, errors.New("unable create order")
+	}
+
+	return &result.Response.Body, nil
+
 }
