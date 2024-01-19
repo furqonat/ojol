20565d2e1cc36cf74396274a8d092c428135edf1
furqonat <furqonr.atthoriq@gmail.com>
2024-01-19 16:43:49 +0700

    add success transaction

--- apps/transactions/routes/transaction.go
+++ apps/transactions/routes/transaction.go
@@ -19,11 +19,13 @@ type TrxRoutes struct {
 func (trxRoutes TrxRoutes) Setup() {
 	trxRoutes.logger.Info("Setting up routes")
 	trxRoutes.handler.Gin.NoRoute(trxRoutes.cors.Cors())
+	trxRoutes.handler.Gin.LoadHTMLFiles("templates/index.html")
 	trxApi := trxRoutes.handler.Gin.Group("/trx").Use(trxRoutes.cors.Cors()).Use(trxRoutes.rateLimitMiddleware.Handle())
 	{
 		trxApi.GET("/", trxRoutes.jwt.HandleAuthWithRoles(utils.ADMIN, utils.SUPERADMIN), trxRoutes.controller.GetTransactions)
 		trxApi.GET("/:id", trxRoutes.jwt.HandleAuthWithRoles(utils.ADMIN, utils.SUPERADMIN), trxRoutes.controller.GetTrx)
 		trxApi.POST("/finish", trxRoutes.controller.FinishOrder)
+		trxApi.GET("/finish", trxRoutes.controller.FinishPayment)
 		trxApi.GET("/merchant", trxRoutes.authMiddleware.HandleAuthWithRoles(utils.MERCHANT), trxRoutes.controller.GetMerchantTrx)
 		trxApi.GET("/driver", trxRoutes.authMiddleware.HandleAuthWithRoles(utils.DRIVER), trxRoutes.controller.GetDriverTrx)
 	}
