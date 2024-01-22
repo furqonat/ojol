e21dbd31686c1b538faa7a558bc696af72f55272
furqonat <furqonr.atthoriq@gmail.com>
2024-01-22 20:05:02 +0700

    add full implementation of search

--- apps/management/app/driver.client.tsx
+++ apps/management/app/driver.client.tsx
@@ -2,12 +2,20 @@
 'use client'
 
 import { useSession } from 'next-auth/react'
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { UrlService } from '../services'
-import { driver, driver_details } from '@prisma/client/users'
-
+import {
+  driver,
+  driver_details,
+  driver_wallet,
+  vehicle,
+} from '@prisma/client/users'
+interface Details extends driver_details {
+  vehicle: vehicle
+}
 interface Driver extends driver {
-  driver_details: driver_details
+  driver_details: Details
+  driver_wallet: driver_wallet
   _count: {
     order: number
   }
@@ -27,7 +35,8 @@ export function Driver() {
       )
         .addQuery('id', 'true')
         .addQuery('name', 'true')
-        .addQuery('driver_details', 'true')
+        .addQuery('driver_details', '{include: {vehicle: true}}')
+        .addQuery('driver_wallet', 'true')
         .addQuery('orderBy', 'order')
         .addQuery('avatar', 'true')
         .addQuery('_count', 'true')
@@ -45,6 +54,7 @@ export function Driver() {
   useEffect(() => {
     fetchDriver().then()
   }, [fetchDriver])
+  console.log(drivers)
 
   return (
     <section>
@@ -101,9 +111,7 @@ export function Driver() {
                       </td>
                       <td>{item._count?.order}</td>
                       <th>
-                        <button className="btn btn-ghost btn-xs">
-                          details
-                        </button>
+                        <DriverDetails data={item} />
                       </th>
                     </tr>
                   )
@@ -113,5 +121,213 @@ export function Driver() {
         </div>
       </section>
     </section>
+  )
+}
+
+function DriverDetails(props: { data: Driver }) {
+  const { data } = props
+  const dialogRef = useRef<HTMLDialogElement>(null)
+
+  function handleOpenDialog(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault()
+    dialogRef.current?.showModal()
+  }
+
+  return (
+    <>
+      <button className={'btn btn-outline'} onClick={handleOpenDialog}>
+        Details
+      </button>
+      <dialog ref={dialogRef} className="modal">
+        <div className="modal-box hide-scrollbar">
+          <h3 className="font-bold text-lg">Driver Details</h3>
+          <div className={'flex flex-col mt-6'}>
+            {/*  */}
+            <div className={'flex flex-col gap-6'}>
+              <h4 className={'font-semibold mb-[-20px]'}>Driver Wallet</h4>
+
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text-alt">Ballance</span>
+                </div>
+                <input
+                  required={true}
+                  type="text"
+                  defaultValue={
+                    Number(data.driver_wallet?.balance)?.toLocaleString(
+                      'id-ID',
+                      {
+                        currency: 'IDR',
+                        style: 'currency',
+                      },
+                    ) ?? '0'
+                  }
+                  readOnly={true}
+                  placeholder="Admin Name"
+                  className="input input-bordered w-full input-sm"
+                />
+              </label>
+              <h4 className={'font-semibold mb-[-20px]'}>Driver Information</h4>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text-alt">Name</span>
+                </div>
+                <input
+                  required={true}
+                  type="text"
+                  defaultValue={data.name ?? ''}
+                  readOnly={true}
+                  placeholder="Admin Name"
+                  className="input input-bordered w-full input-sm"
+                />
+              </label>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text-alt">Address</span>
+                </div>
+                <input
+                  required={true}
+                  type="text"
+                  defaultValue={data.driver_details?.address}
+                  readOnly={true}
+                  placeholder="Admin Name"
+                  className="input input-bordered w-full input-sm"
+                />
+              </label>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text-alt">Phone Number</span>
+                </div>
+                <input
+                  required={true}
+                  type="text"
+                  defaultValue={data.phone ?? ''}
+                  readOnly={true}
+                  placeholder="Admin Name"
+                  className="input input-bordered w-full input-sm"
+                />
+              </label>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text-alt">Email</span>
+                </div>
+                <input
+                  required={true}
+                  type="text"
+                  defaultValue={data.email ?? ''}
+                  readOnly={true}
+                  placeholder="Admin Name"
+                  className="input input-bordered w-full input-sm"
+                />
+              </label>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text-alt">ID Card Image</span>
+                </div>
+                <img
+                  src={data.driver_details?.id_card_image ?? ''}
+                  className={'rounded-md'}
+                  alt={'id card image'}
+                />
+              </label>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text-alt">License Image</span>
+                </div>
+                <img
+                  src={data.driver_details?.license_image ?? ''}
+                  className={'rounded-md'}
+                  alt={'id card image'}
+                />
+              </label>
+              <h4 className={'font-semibold mb-[-20px]'}>
+                Vehicle Information
+              </h4>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text-alt">Vehicle Brand</span>
+                </div>
+                <input
+                  required={true}
+                  type="text"
+                  defaultValue={
+                    data.driver_details?.vehicle.vehicle_brand ?? ''
+                  }
+                  readOnly={true}
+                  placeholder="Admin Name"
+                  className="input input-bordered w-full input-sm"
+                />
+              </label>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text-alt">Vehicle Type</span>
+                </div>
+                <input
+                  required={true}
+                  type="text"
+                  defaultValue={data.driver_details?.vehicle.vehicle_type ?? ''}
+                  readOnly={true}
+                  placeholder="Admin Name"
+                  className="input input-bordered w-full input-sm"
+                />
+              </label>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text-alt">Vehicle Year</span>
+                </div>
+                <input
+                  required={true}
+                  type="text"
+                  defaultValue={data.driver_details?.vehicle.vehicle_year ?? ''}
+                  readOnly={true}
+                  placeholder="Admin Name"
+                  className="input input-bordered w-full input-sm"
+                />
+              </label>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text-alt">
+                    Vehicle Registration Number
+                  </span>
+                </div>
+                <input
+                  required={true}
+                  type="text"
+                  defaultValue={data.driver_details?.vehicle.vehicle_rn ?? ''}
+                  readOnly={true}
+                  placeholder="Admin Name"
+                  className="input input-bordered w-full input-sm"
+                />
+              </label>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text-alt">Vehicle Image</span>
+                </div>
+                <img
+                  src={data.driver_details?.vehicle.vehicle_image ?? ''}
+                  className={'rounded-md'}
+                  alt={'id card image'}
+                />
+              </label>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text-alt">Vehicle Registration</span>
+                </div>
+                <img
+                  src={data.driver_details?.vehicle.vehicle_registration ?? ''}
+                  className={'rounded-md'}
+                  alt={'id card image'}
+                />
+              </label>
+            </div>
+          </div>
+          <div className="modal-action">
+            <form method="dialog" className={'flex gap-6'}>
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </>
   )
 }
